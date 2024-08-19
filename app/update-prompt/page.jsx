b12743c9ce1b4ba901a8baa13462dcb03f5d0dc2
caspianbryan <brianberrieron@gmail.com>
2024-08-19 15:23:46 +0300

    errors resolved

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,75 +1,66 @@
-'use client'
+"use client";
 
-import { Suspense, useEffect, useState } from "react"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
+import Form from "@components/Form";
 
-import Form from "@components/Form"
+const UpdatePrompt = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get("id");
 
-const EditPrompt = () => {
-
-    const router = useRouter()
-
-    const searchParams = useSearchParams()
-    const promptId = searchParams.get('id')
-
-    const [submitting, setSubmitting] = useState(false)
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: ''
-    })
+    const [post, setPost] = useState({ prompt: "", tag: "", });
+    const [submitting, setIsSubmitting] = useState(false);
 
     useEffect(() => {
         const getPromptDetails = async () => {
-            const response = await fetch(`/api/prompt/${promptId}`)
-            const data = await response.json()
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = await response.json();
 
             setPost({
                 prompt: data.prompt,
-                tag: data.tag
-            })
-        }
+                tag: data.tag,
+            });
+        };
 
-        if (promptId) getPromptDetails()
-
-    }, [promptId])
+        if (promptId) getPromptDetails();
+    }, [promptId]);
 
     const updatePrompt = async (e) => {
         e.preventDefault();
-        setSubmitting(true);
+        setIsSubmitting(true);
 
-        if (!promptId) return alert('Prompt Id not found')
+        if (!promptId) return alert("Missing PromptId!");
 
         try {
             const response = await fetch(`/api/prompt/${promptId}`, {
-                method: 'PATCH',
+                method: "PATCH",
                 body: JSON.stringify({
                     prompt: post.prompt,
-                    tag: post.tag
-                })
-            })
+                    tag: post.tag,
+                }),
+            });
 
             if (response.ok) {
-                router.push('/');
+                router.push("/");
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         } finally {
-            setSubmitting(false)
+            setIsSubmitting(false);
         }
-    }
+    };
 
     return (
-        <Suspense fallback={<div>Loading....</div>}>
-            <Form
-                type="Edit"
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-            />
-        </Suspense>
-    )
-}
+        <Form
+            type='Edit'
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
+    );
+};
 
-export default EditPrompt
+export default UpdatePrompt;
--- utils/database.js
+++ utils/database.js
@@ -1,35 +1,24 @@
 import mongoose from "mongoose";
 
-let isConnected = false // track the connection
+let isConnected = false; // track the connection
 
 export const connectedToDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set("strictQuery", true);
 
-    if(isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'prompts'
-        })
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
 
-        isConnected = true
-        // const mongoURI = process.env.MONGODB_URI;
-        // if (!mongoURI) {
-        //     throw new Error('MONGODB_URI is not defined');
-        // }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "prompts"
+    });
 
-        // await mongoose.connect(mongoURI, {
-        //     dbName: 'prompts',
-        //     // Removed deprecated options
-        // });
+    isConnected = true;
 
-        // isConnected = true;
-        console.log('MongoDB Connected');
-        
-    } catch (error) {
-        console.log(error);
-        throw error
-    }
-}
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
